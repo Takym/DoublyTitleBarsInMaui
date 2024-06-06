d7208d99d8bd8ed9b4722b3e28619ff387f3c6be
Takym <15681312+Takym@users.noreply.github.com>
2024-06-07 08:20:41 +0900

    プロジェクト ファイルを追加します。

---
+++ DoublyTitleBarsInMaui/MauiProgram.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+
+#if WINDOWS
+using Microsoft.Maui.LifecycleEvents;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+#endif
+
+namespace DoublyTitleBarsInMaui
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts => {
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				});
+
+#if DEBUG
+			builder.Logging.AddDebug();
+#endif
+
+#if WINDOWS
+			builder.ConfigureLifecycleEvents(
+				builder => builder.AddWindows(
+					builder => builder.OnWindowCreated(window => {
+						window.AppWindow.TitleBar.ExtendsContentIntoTitleBar = false;
+
+#if WORKAROUND
+						// Workaround:
+						if (window.Content is FrameworkElement elem) {
+							elem.Margin = new(0, -32, 0, 0);
+							Canvas.SetZIndex(elem, 0);
+						}
+#endif // WORKAROUND
+					})
+				)
+			);
+#endif // WINDOWS
+
+			return builder.Build();
+		}
+	}
+}
---
+++ DoublyTitleBarsInMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DoublyTitleBarsInMaui
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
