a4290239443bd26ef5ebf2f2eb2cc67825126a41
Takym <15681312+Takym@users.noreply.github.com>
2024-11-25 13:20:06 +0900

    Update Runtime

--- DoublyTitleBarsInMaui/App.xaml.cs
+++ DoublyTitleBarsInMaui/App.xaml.cs
@@ -4,9 +4,21 @@
 	{
 		public App()
 		{
-			InitializeComponent();
+			this.InitializeComponent();
 
-			MainPage = new AppShell();
+			//this.MainPage = new AppShell();
+		}
+
+		protected override Window CreateWindow(IActivationState? activationState)
+		{
+			var page = new AppShell();
+
+			if (this.Windows.Count >= 1) {
+				this.Windows[0].Page = page;
+				return base.CreateWindow(activationState);
+			}
+
+			return new(page);
 		}
 	}
 }
